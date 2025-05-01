93c970f448a0e172671df968e75a31e2b2aa99cc
salmanOFC <salmansaeedofc@gmail.com>
2025-05-01 16:05:33 +0500

    updated reconstruct buffer for inter module

--- rtl/h264recon.sv
+++ rtl/h264recon.sv
@@ -1,6 +1,9 @@
 module h264recon
 (
     input logic CLK2,				// x2 clock
+    // interface for inter
+    input logic inter_flag_valid,
+    input logic inter_flag,
     // in interface:
     input logic NEWSLICE,			// reset
     input logic STROBEI,		    // data here
@@ -14,12 +17,12 @@ module h264recon
     output logic [31:0] DATAO = '0
 );
 
-    logic [31:0] basevec [7:0] = '{default: '0};
+    logic [31:0] basevec [63:0] = '{default: '0};
 
-    logic [1:0] chromaf;
+    logic [63:0] chromaf;
     logic [31:0] basex;
-    logic [3:0] basein = '0;
-    logic [3:0] baseout = '0;
+    logic [5:0] basein = '0;
+    logic [5:0] baseout = '0;
     logic [9:0] byte0 = '0;
     logic [9:0] byte1 = '0;
     logic [9:0] byte2 = '0;
@@ -27,35 +30,84 @@ module h264recon
     logic strobex = 0;
     logic chromax = 0;
 
-    assign basex = basevec[baseout[2:0]];
+    logic basein_rst;
+    logic baseout_rst;
+
+    assign basein_rst = (basein == 6'b111111);
+    assign baseout_rst = (baseout == 6'b111111);
+
+    // assign basex = basevec[baseout[2:0]];
+    always_comb 
+    begin
+        if (inter_flag_valid)
+        begin
+            if (inter_flag)
+                basex = basevec[baseout[5:0]];
+            else
+                basex = basevec[baseout[2:0]];
+        end
+    end
     
     always@(posedge CLK2) 
     begin
 
-        if (NEWSLICE) 
+        if (NEWSLICE || !inter_flag_valid) 
         begin    // reset
-			basein <= 4'd0;
-			baseout <= 4'd0;
+			basein <= 6'd0;
+			baseout <= 6'd0;
 		end
 
-		// load in base
+        // -- Counter Reset on Limit --
+        if (basein_rst)
+        begin
+            basein <= 6'd0;
+        end
+
+        if (baseout_rst)
+        begin
+            baseout <= 6'd0;
+        end
+        // -----------------------------
+
+		// -- Load LUMA/CHROMA BASE (REFERENCE) -- 
 		if (BSTROBEI && !NEWSLICE) 
         begin
-			basevec[basein[2:0]] <= BASEI;
-			chromaf[basein[2]] <= BCHROMAI;
-			basein <= basein + 1;
-			assert (basein+8 != baseout) else $error("basein wrapped");
+            if (inter_flag)
+            begin
+                basevec[basein] <= BASEI;
+                chromaf[basein] <= BCHROMAI;
+                basein <= basein + 1;
+            end
+            else
+            begin
+                basevec[basein[2:0]] <= BASEI;
+                chromaf[basein[2]] <= BCHROMAI;
+                basein <= basein + 1;
+                assert (basein+8 != baseout) else $error("basein wrapped");
+            end
         end
 		else
 			assert (basein[1:0] == '0) else $error("basein not aligned when strobe falls");
+        // -----------------------------
 
-        // reconstruct +0: add
+        // -- RECONSTRUCT: REFERENCE(BASE) + RESIDUAL(DATA) --
         byte0 <= {2'b00, basex[7:0]} + DATAI[9:0];
 		byte1 <= {2'b00, basex[15:8]} + DATAI[19:10];
 		byte2 <= {2'b00, basex[23:16]} + DATAI[29:20];
 		byte3 <= {2'b00, basex[31:24]} + DATAI[39:30];
+        // -----------------------------
 
-        chromax <= chromaf[baseout[2]];
+
+        // Select if CHROMA PIXELS
+        if (inter_flag)
+        begin
+            chromax <= chromaf[baseout];
+        end
+        else
+        begin
+            chromax <= chromaf[baseout[2]];
+        end
+
         strobex <= STROBEI;
 
         if (STROBEI && !NEWSLICE) 
